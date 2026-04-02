52fc26a22ffa87032dbb0d10de04a8a3d8069dfc
KHALED MD MEHZABIN ALAM PROTTOY <khaledmdprottoy@gmail.com>
2026-04-02 23:43:31 +0600

    Delete app directory

--- app/sitemap.ts
+++
@@ -1,9 +0,0 @@
-import type { MetadataRoute } from "next";
-import { navItems, secondaryNav, site } from "@/lib/site";
-
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [...navItems, ...secondaryNav].map((item) => ({
-    url: `${site.url}${item.href}`,
-    lastModified: new Date(),
-  }));
-}
